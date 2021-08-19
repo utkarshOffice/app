6e9590a58d260987bc149954575d72582f7fae03
Abhishek Hegde <88656679+abhis-codes@users.noreply.github.com>
2021-08-19 11:56:53 +0530

    Updated comments

--- Server_Modules/chillrollServer.R
+++ Server_Modules/chillrollServer.R
@@ -3,10 +3,9 @@ chillrollServer <- function(id, top_session){
     id,
     function(input, output, session) {
       
+      # initializing some variables which will be used later
       santosh_proxy <- DT::dataTableProxy('optimiser_table1_santosh')
       opt<-reactiveValues(tab_1=NULL) 
-      
-      
       manualinput_santosh <- reactiveVal(NULL)
       manual_santosh <- reactiveVal(NULL)
       importresults_santosh <- reactiveVal(NULL)
@@ -14,35 +13,40 @@ chillrollServer <- function(id, top_session){
       optimise1_santosh <- reactiveVal(NULL)
       optimise2_santosh <- reactiveVal(NULL)
       
+      
+      # ---------------------------------------------------- MODEL & DATA IMPORT --------------------------------------------------------
+      
+      # making a dataframe for our two models calculating Flake Final Temp (Model 1 & 2) 
       x_santosh <- data.frame(Models_santosh <- c("Flake Final Temp (Model 1) = (Cooling_seg_fraction)*(-45.5179701273954) + (Film_thickness)*(18.8956339477401) + ((Film_thickness)^2)*(6.67710528547635) + (Roll_Speed)*(7.21746209125386) + ((Roll_Speed)^2)*(1.27090332399397) + 20.3887470216993",
                                             "Flake Final Temp (Model 2) = (Cooling_seg_fraction)*(-48.3757602771176) + (T_ambient)^2*(-0.0000431916784640458) + (T_flake feed)*(0.339086502336415) + (T_chilled water)*0.529509608669907 + (Film thickness)*(18.6709859995675) + (Film thickness)^2*(6.55116697694696) + (DEFI Free roll length)^2*(0.13197883988453) + (Roll Speed)*7.21191328015338 + (Roll Speed)^2*1.26186094993258 - 18.9553266806959"
                                             ))
       
-      
+      # lists for variable names
       b_santosh <- c("Film_thickness", "Cooling_seg_fraction", "T_flake_feed" , "T_ambient","T_chilled_water" ,"DEFI_Free_roll_length", "Roll_Speed")
+      all_vars_santosh <-c("Film_thickness", "Cooling_seg_fraction", "T_flake_feed" , "T_ambient","T_chilled_water" ,"DEFI_Free_roll_length", "Roll_Speed")
+      
+      # lists for excel file column names
       c_santosh <- c("filmthick", "coolseg", "tfeed" , "tambient","tsteel" ,"rollldefifree", "rollspeed")
       d_santosh <- c("filmthick", "coolseg", "tfeed" , "tambient","tsteel" ,"rollldefifree", "rollspeed")
-      # c_santosh <- c("targetsmc", "NaLASinSlurry", "LSAinSlurry" , "SulphateinSlurry","AlkSilicateinSlurry" ,"CP5inSlurry", "SCMCinSlurry")
-      # d_santosh <- c("targetsmc", "nalas", "lsa" , "sulphate","alksilicate" ,"cp5", "scmc")
-      all_vars_santosh <-c("Film_thickness", "Cooling_seg_fraction", "T_flake_feed" , "T_ambient","T_chilled_water" ,"DEFI_Free_roll_length", "Roll_Speed")
-      # model bank table 
+     
       
+      # model bank table output
       output$models_santosh <- renderDataTable({
         datatable(x_santosh, colnames = c("Models"))
       } )
       
       
-      
-      # go to simulation
+      # button action to go to simulation
       observeEvent(input$commit_santosh,{
         updateTabsetPanel(top_session, "tabs_santosh", selected = "Simulation")
       })
-      # go to visualization
+      
+      # button action to go to visualization
       observeEvent(input$commit2_santosh,{
         updateTabsetPanel(top_session, "tabs_santosh", selected = "Visualization")
       })
       
-      #rendering advisory tables
+      # rendering advisory tables - predictors and their limits
       output$advice_santosh <- renderDataTable({
         Advisory_table <- data.frame(Ingredients = c("Film thickness","Cooling_seg_fraction",
                                                      "T_flake feed",
@@ -56,6 +60,7 @@ chillrollServer <- function(id, top_session){
       
       colors <- c("red","black", "green","yellow","violet")
       
+      # ------------------------------------------------ SIMULATION - PROFILER ------------------------------------------------------
       
       # profiler renderings
       observeEvent(req(input$profiler_Film_thickness),{
@@ -87,15 +92,15 @@ chillrollServer <- function(id, top_session){
             Final_Flake_Temp_M2 <- reactive(eval(parse(text = eqn2)))
             
             output$plot1 <- renderPlot({
-              FFT1 <- Final_Flake_Temp_M1()
-              ggplot(data=data.frame(Film_thickness, FFT1), aes(x=Film_thickness, y= FFT1)) +
+              Final_Flake_Temp_1 <- Final_Flake_Temp_M1()
+              ggplot(data=data.frame(Film_thickness, Final_Flake_Temp_1), aes(x=Film_thickness, y= Final_Flake_Temp_1)) +
                 geom_line() + geom_point(size = 4)+ theme(text = element_text(size = 20))+
                 gghighlight(Film_thickness == input$profiler_Film_thickness)
             })
             
             output$plot2 <- renderPlot({
-              FFT2 <- Final_Flake_Temp_M2()
-              ggplot(data=data.frame(Film_thickness, FFT2), aes(x=Film_thickness, y= FFT2)) +
+              Final_Flake_Temp_2 <- Final_Flake_Temp_M2()
+              ggplot(data=data.frame(Film_thickness, Final_Flake_Temp_2), aes(x=Film_thickness, y= Final_Flake_Temp_2)) +
                 geom_line() + geom_point(size = 4)+ theme(text = element_text(size = 20))+
                 gghighlight(Film_thickness == input$profiler_Film_thickness)
             })
@@ -108,14 +113,12 @@ chillrollServer <- function(id, top_session){
       
       #visualization page renderings
       observeEvent(req(input$datacall_santosh),
-                   {#data_santosh <- reactive(read_excel(input$datacall_santosh$datapath))
-                     #data <- reactive(data()[,-(which(colSums(data())==0))])
+                   {
                      santosh_data_slurry1 <- reactive({
                        req(input$datacall_santosh)
                        inFile <- input$datacall_santosh
                        if(is.null(inFile)) return(NULL)
 
-                       #xlfile <- read_excel(paste0(inFile$datapath, ".xlsx"))
                        xlfile <- read_excel(input$datacall_santosh$datapath , sheet='Sensor')
                        colnames(xlfile) <- gsub("\\[.*?\\]","",colnames(xlfile))
                        colnames(xlfile) <- gsub(" ","",colnames(xlfile))
@@ -124,39 +127,26 @@ chillrollServer <- function(id, top_session){
 
                      })
                      df1 <- santosh_data_slurry1()
-                     # df1$sum <- df1$nalas + df1$scmc + df1$alksilicate + df1$cp5 + df1$sulphate + df1$lsa
-                     # df1$NaLASinSlurry <- df1$nalas/df1$sum*(1-df1$targetsmc)
-                     # df1$LSAinSlurry <- df1$lsa/df1$sum*(1-df1$targetsmc)
-                     # df1$SCMCinSlurry <- df1$scmc/df1$sum*(1-df1$targetsmc)
-                     # df1$AlkSilicateinSlurry <- df1$alksilicate/df1$sum*(1-df1$targetsmc)
-                     # df1$CP5inSlurry <- df1$cp5/df1$sum*(1-df1$targetsmc)
-                     # df1$SulphateinSlurry <- df1$sulphate/df1$sum*(1-df1$targetsmc)
-                     #View(df1)
+                     
                      santosh_data_slurry2 <- reactive({
                        req(input$datacall_santosh)
                        inFile <- input$datacall_santosh
                        if(is.null(inFile)) return(NULL)
 
-                       #file.rename(inFile$datapath, paste0(inFile$datapath, ".xlsx"))
-                       #xlfile <- read_excel(paste0(inFile$datapath, ".xlsx"))
+                       # reading only range in which predictors exist
                        xlfile <- read_excel(input$datacall_santosh$datapath, sheet='Sensor', range='Sensor!D1:J502')
 
                        xlfile
 
                      })
                      df2 <- santosh_data_slurry2()
-                     #df3 <- df1[tail(seq_along(df1),7)]
                      
-                     #colnames(df3) <- c("Cool seg",	"T ambient","T feed","T steel",	"film thick"	,"roll L DEFI Free", "roll Speed")
-                     #View(df3)
-                     #finaldf <- as.data.frame(cbind(df2, df3))
                      finaldf <- as.data.frame(df2)
 
                      output$simulationdata_santosh <- renderDataTable({
                        santosh_data_slurry2()
                      })
-                     # View(finaldf)
-                     #View(data_santosh)
+                     
                      updateSelectInput(session, "y_axis_bd", choices = colnames(finaldf), selected = colnames(finaldf)[1])
                      updateSelectInput(session, "x_axis_bd", choices = colnames(finaldf), selected = colnames(finaldf)[5])
                      updateSelectInput(session, "x_line", choices = colnames(finaldf))
@@ -188,9 +178,7 @@ chillrollServer <- function(id, top_session){
                            ggplot(finaldf, aes(x= finaldf[[input$x_axis_bd]],y= finaldf[[input$y_axis_bd]])) +
                              geom_point(col="blue") + geom_smooth(method="lm", col="blue") +xlab(as.character(input$x_axis_bd))+
                              ylab(as.character(input$y_axis_bd))
-                           #labs(x = as.character(input$x_axis_bd),y = as.character(input$y_axis))
                          }else{
-                           #scaleFactor <- max(data()[[input$y_axis[1]]]) / max(data()[[input$y_axis[2]]])
                            first <- input$y_axis_bd[1]
                            second <- input$y_axis_bd[2]
 
@@ -212,12 +200,10 @@ chillrollServer <- function(id, top_session){
                            ggplot(data_santosh_bd(), aes(x= finaldf[[input$x_axis_bd]],y= finaldf[[input$y_axis_bd]])) +
                              geom_point() +xlab(as.character(input$x_axis_bd))+
                              ylab(as.character(input$y_axis_bd))
-                           # labs(x = as.character(input$x_axis_bd),y = as.character(input$y_axis))
                          }else
                          {
                            first <- input$y_axis_bd[1]
                            second <- input$y_axis_bd[2]
-                           #scaleFactor <- max(data()[[first]]) / max(data()[[second]])
 
                            ggplot(finaldf, aes(x=finaldf[[input$x_axis_bd]])) +
                              geom_point(aes(y=finaldf[[first]]), col="blue")+
@@ -239,7 +225,6 @@ chillrollServer <- function(id, top_session){
                            ggplot(finaldf, aes(x= finaldf[[input$x_axis_bd]],y= finaldf[[input$y_axis_bd]])) +
                              geom_point(col="blue") + geom_line(col="blue") +labs(x = as.character(input$x_axis_bd))
                          }else{
-                           #scaleFactor <- max(data()[[input$y_axis[1]]]) / max(data()[[input$y_axis[2]]])
                            first <- input$y_axis_bd[1]
                            second <- input$y_axis_bd[2]
 
@@ -248,8 +233,7 @@ chillrollServer <- function(id, top_session){
                              geom_point(aes(y=finaldf[[second]] * 1), col="red", shape = 18)+
                              geom_line(aes(y=finaldf[[first]]), col="blue")+
                              geom_line(aes(y=finaldf[[second]] * 1), col="red")+
-                             # geom_smooth(aes(y=data()[[first]]), method="loess", col="blue") +
-                             # geom_smooth(aes(y=data()[[second]] * 20), method="loess", col="red") +
+                           
                              scale_y_continuous(name=as.character(first), sec.axis=sec_axis(~./1, name=as.character(second))) +
                              theme(
                                axis.title.y.left=element_text(color="blue"),
@@ -266,7 +250,6 @@ chillrollServer <- function(id, top_session){
                          {
                            first <- input$y_axis_bd[1]
                            second <- input$y_axis_bd[2]
-                           #scaleFactor <- max(data()[[first]]) / max(data()[[second]])
 
                            ggplot(finaldf, aes(x=finaldf[[input$x_axis_bd]])) +
                              geom_point(aes(y=finaldf[[first]]), col="blue")+
@@ -339,7 +322,7 @@ chillrollServer <- function(id, top_session){
                    })
 
       
-      # --------------------------------------------- SIMULATION ------------------------------------------------------
+      # --------------------------------------------- SIMULATION - MANUAL ENTRY --------------------------------------------------
       
       observeEvent(req(x_santosh),{
         x1_santosh <- reactiveValues()
@@ -386,6 +369,8 @@ chillrollServer <- function(id, top_session){
         proxy_santosh <- dataTableProxy("simulation_input_santosh")
         
         
+        # to capture edited values in our saved table - x1_santosh
+        
         observeEvent(input[["simulation_input_santosh_cell_edit"]], {
           info <- input[["simulation_input_santosh_cell_edit"]]
           
@@ -415,7 +400,7 @@ chillrollServer <- function(id, top_session){
           
           df <- as.data.frame(x1_santosh$df)
   
-          
+          # prefixing 'df' to column names
           for(i in b_santosh){
             eqn1 <- gsub(i, df[1,i], eqn1)
             eqn2 <- gsub(i, df[1,i], eqn2)
@@ -425,8 +410,10 @@ chillrollServer <- function(id, top_session){
           Final_Flake_Temp_M1 <- eval(parse(text = eqn1))
           Final_Flake_Temp_M2 <- eval(parse(text = eqn2))
 
-          
+          # result table
           tbl <- cbind(Final_Flake_Temp_M1, Final_Flake_Temp_M2)
+          
+          # input table
           df1 <- x1_santosh$df
           
           manualinput_santosh(df1)
@@ -434,6 +421,7 @@ chillrollServer <- function(id, top_session){
           
           nrdata <- as.data.frame(tbl)
           nrdata1 <- as.data.frame(df)
+          
           output$download1_santosh <- downloadHandler(
             filename = function() { "Manual Entry Simulation.xlsx"},
             content = function(file) {
@@ -450,13 +438,13 @@ chillrollServer <- function(id, top_session){
         })
         
         
+        # ---------------------------------------- SIMULATION - IMPORTED DATA -------------------------------------------------
         
         santosh_data_slurry <- reactive({
           req(input$datacall_santosh)
           inFile <- input$datacall_santosh
           if(is.null(inFile)) return(NULL)
-          
-
+        
           xlfile <- read_excel(input$datacall_santosh$datapath, sheet='Sensor')
           colnames(xlfile) <- gsub("\\[.*?\\]","",colnames(xlfile))
           colnames(xlfile) <- gsub(" ","",colnames(xlfile))
@@ -465,6 +453,7 @@ chillrollServer <- function(id, top_session){
           
         })
         
+        # checking whether data contains all required columns
         observeEvent(santosh_data_slurry(),
                      {i <- d_santosh[!is.element(d_santosh,colnames(santosh_data_slurry()))]
                      if(length(i)>=1){
@@ -482,7 +471,7 @@ chillrollServer <- function(id, top_session){
             eqn2 <- "(coolseg)*(-48.3757602771176) + ((tambient)**2)*(-0.0000431916784640458) + (tfeed)*(0.339086502336415) + (tsteel)*(0.529509608669907) + (filmthick)*(18.6709859995675) + ((filmthick)**2)*(6.55116697694696) + ((rollldefifree)**2)*(0.13197883988453) + (rollspeed)*(7.21191328015338) + ((rollspeed)**2)*(1.26186094993258) - (18.9553266806959)"
             
 
-            
+            # prefixing 'df1' to column_names in equations
             for(i in c_santosh){
               eqn1 <- gsub(i, paste0("df1$",i), eqn1)
               eqn2 <- gsub(i, paste0("df1$",i), eqn2)
@@ -493,10 +482,8 @@ chillrollServer <- function(id, top_session){
             Final_Flake_Temp_M2 <- eval(parse(text = eqn2))
 
             tbl <- cbind(Final_Flake_Temp_M1, Final_Flake_Temp_M2)
+            
             nrdata <- as.data.frame(tbl)
-            
-         
-            
             importresults_santosh(tbl)
 
             output$download2_santosh <- downloadHandler(
@@ -518,10 +505,8 @@ chillrollServer <- function(id, top_session){
         
         
         
-        
-        
-        #})
         # --------------------------------------------- OPTIMIZATION ------------------------------------------------------
+        
         #optimisation renderings for santosh(SD slurry props- drying prediction)
         observeEvent(req(x_santosh),
                      {
