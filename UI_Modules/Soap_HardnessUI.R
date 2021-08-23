4a7d3959eb391aec7bc3777a36349ebdb08487f9
utkarshOffice <86705453+utkarshOffice@users.noreply.github.com>
2021-08-23 15:19:19 +0530

    Create Soap_HardnessUI.R
    adding soap hardness ui

---
+++ UI_Modules/Soap_HardnessUI.R
@@ -0,0 +1,151 @@
+Soap_HardnessUI <- function(id){
+  ns <- NS(id)
+  
+  tagList(
+    {
+      tabsetPanel(id = "tabs_uday_sd",tabPanel("Model Introduction",
+                                               h1("Soap Bar Performance Models - Hardness"),
+                                               em("Model Owner: Pavitra")),
+                  tabPanel("Model & Data Import",
+                           wellPanel(
+                             # h1("Finished Goods Bulk Density(SD Powder) Model"),
+                             # em("Model Owner: Uday Baviskar"),
+                             h2("Available Model Outputs"),
+                             tags$ul(
+                               tags$li(" This table presents the available models for this segment.")
+                               ,tags$li(" Simulation will take place for all equations simultaneously.")
+                             ),
+                             dataTableOutput(ns("models_uday_sd"))
+                           )
+                           ,
+                           wellPanel(
+                             h2("Simulation Data Inputs"),
+                             p("Only one of manual data entry and external import is allowed. Select one and proceed."),
+                             tabsetPanel(tabPanel("Manual Entry",
+                                                  tags$ul(
+                                                    tags$li(" Following table is editable. Enter the simulation values."),
+                                                    tags$li(" Entered values will automatically be saved for simulation stage.")
+                                                  ),
+                                                  dataTableOutput(ns("simulation_input_uday_sd")),
+                                                  actionButton(ns("commit_uday_sd"),"Go to simulation",icon("paper-plane"), 
+                                                               style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+                                                  br(),
+                             ),
+                             tabPanel("External Data Import",
+                                      tags$ul(
+                                        tags$li(" This segment provides simulation data import functionality."),
+                                        tags$li(" A time series excel file dataset is expected with the naming format of TurningPoint equation.")
+                                      ),
+                                      fileInput(ns("datacall_uday_sd"),"Import Simulation Data"),
+                                      dataTableOutput(ns("simulationdata_uday_sd")),
+                                      actionButton(ns("commit2_uday_sd"), "Go to Visualization",icon("paper-plane"), 
+                                                   style="color: #fff; background-color: #337ab7; border-color: #2e6da4"))
+                             ),
+                           )),
+                  tabPanel("Visualization",
+                           fluidRow(
+                             wellPanel(
+                               h4("Visual guide for imported data."),
+                               tags$ul(
+                                 tags$li(" This tab presents visualizations of the externally imported data."),
+                                 tags$li(" First two graphs allow overlapping of two variables on Y axis."),
+                                 tags$li(" While the first graph presents linear trend through line graph, the second graph presents linear regression based trend in data."),
+                                 tags$li(" Third graph shows scatter plot as well as trend lines. Upto 4 variables are allowed in this graph on Y axis."),
+                                 tags$li(" Finally a histogram is presented.")
+                               ),
+                             ),
+                             wellPanel(
+                               h2("Scatter Plot"),
+                               tags$ul(
+                                 tags$li(" This scatter graph allows for multiple Y axis selection on single x axis."),
+                                 tags$li(" Maximum 4 selections are allowed for Y axis. Linear regression method is used for creating trendlines.")
+                               ),
+                               selectInput(ns("x_axis_sd"), 'X axis', choices = c(" "," ") ),
+                               selectInput(ns("y_axis_sd"), 'Y axis', choices = c(" "," ")
+                                           ,selected = names(mtcars)[2], multiple = TRUE),
+                               plotOutput(ns("scatterplot_uday_sd")),
+                               br(),
+                               br(),
+                               tags$ul(
+                                 tags$li(" This scatter graph allows for double Y axis selection on single x axis."),
+                                 tags$li(" It takes the first two selections of y-axis for plotting . Linear regression method is used for creating trendlines."),
+                                 tags$li("Trend Line checkbox can be adding or removing the trend line in graph.")
+                               ),
+                               checkboxInput(ns("smooth_uday_sd"),"Trend Line", TRUE),
+                               plotOutput(ns("ggscatter_uday_sd")),
+                               checkboxInput(ns("smooth2_uday_sd"),"Multitrend", TRUE),
+                               plotOutput(ns("ggsmooth_uday_sd"))
+                             ),
+                             wellPanel(
+                               h2("Histogram"),
+                               selectInput(ns("hist_choice_uday_sd"), "Histogram Variable", c(" "," ")),
+                               plotOutput(ns("hist_uday_sd"))
+                             )
+                           ),
+                  ),
+                  tabPanel("Simulation",
+                           h3("Simulation Input Response"),
+                           tabsetPanel(id = "simuPage_uday_sd",
+                                       tabPanel("Manual Entry Simulation",
+                                                h3("Input values for manual Simulation"),
+                                                dataTableOutput(ns("modeltable_uday_sd")),
+                                                actionButton(ns("simulate_uday_sd"),"Simulate on entered Data",style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+                                                uiOutput(ns("simulation_result_uday_sd")),
+                                                dataTableOutput(ns("result1_uday_sd")),
+                                                downloadButton(ns("download1_uday_sd"),"Download above result",style="color: #fff; background-color: #337ab7; border-color: #2e6da4")
+                                       ),
+                                       
+                                       tabPanel("Profiler",
+                                                wellPanel(
+                                                  h1("Profiler"),
+                                                  fluidRow(column(width = 10,plotOutput(ns("plot1"))),
+                                                  ),
+                                                  
+                                                  fluidRow(column(width = 6,
+                                                                  sliderInput(ns("billetmoistur"),"Billet Moisture:", min = 0, max = 100, value = 1),
+                                                                  sliderInput(ns("plodderbackpressuredegc"),"Plodder Back Pressure (30%) @ 40 degC:", min = 0, max = 100, value = 1),
+                                                                  sliderInput(ns("flowrate"),"Flow Rate (30%):", min = 0, max = 100, value = 1),
+                                                                  sliderInput(ns("moisture"),"Moisture:", min = 0, max = 100, value = 1),
+                                                                  sliderInput(ns("pkocontent"),"PKO Content:", min = 0, max = 100, value = 1),
+                                                                  sliderInput(ns("glycerine"),"Glycerine:", min = 0, max = 100, value = 1),
+                                                                  sliderInput(ns("carbopolesc"),"Carbopole SC200 (100%):", min = 0, max = 100, value = 1),
+                                                                  sliderInput(ns("sodiumchloride"),"Sodium Chloride:", min = 0, max = 100, value = 1)
+                                                  ),
+                                                  column(width = 6,
+                                                         sliderInput(ns("soapmasstemperature"),"Soap Mass Temperature:", min = 0, max = 100, value = 1),
+                                                         sliderInput(ns("iv"),"IV (Iodine Value):", min = 0, max = 100, value = 1),
+                                                         sliderInput(ns("sodiumsulfate"),"Sodium Sulfate:", min = 0, max = 100, value = 1),
+                                                         sliderInput(ns("sodiumsilicate"),"Sodium Silicate:", min = 0, max = 100, value = 1),
+                                                         sliderInput(ns("petrolatumjelly"),"Petrolatum Jelly:", min = 0, max = 100, value = 1),
+                                                         sliderInput(ns("amazonpolymer"),"Amazon Polymer:", min = 0, max = 100, value = 1),
+                                                         sliderInput(ns("talc"),"Talc:", min = 0, max = 100, value = 1)
+                                                  ))
+                                                )),
+                                       tabPanel("Imported Data Simulation",
+                                                uiOutput(ns("simulation_result_uday_sd2")),
+                                                column(4,dataTableOutput(ns("modeltable2_uday_sd"))),
+                                                br(),
+                                                actionButton(ns("simulate2_uday_sd"),"Simulate on Imported Data",style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+                                                downloadButton(ns("download2_uday_sd"),"Download above result",style="color: #fff; background-color: #337ab7; border-color: #2e6da4")
+                                                
+                                                
+                                       ))
+                           
+                  ),
+                  tabPanel("Optimization", 
+                           h2("Since the equation is non-linear, optimization is pending."),
+                           wellPanel(
+                             h2("Global Download"),
+                             h4("Download all the results that have been generated throughout the app"),
+                             actionButton(ns("downloadresults_uday_sd"),"Proceed to download all Results", 
+                                          style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+                             uiOutput(ns("Download_Values_uday_sd"))
+                             
+                             
+                             #downloadButton(ns("download_all"),"Download above result",style="color: #fff; background-color: #337ab7; border-color: #2e6da4"),
+                             
+                           ))
+      )
+    }
+  )
+}
